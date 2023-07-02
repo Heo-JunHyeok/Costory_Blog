bab3e53112321d89ff900acbe2d5176259d56c3a
Heo-JunHyeok <junhyeok_1996@naver.com>
2023-07-03 03:18:58 +0900

    Form에 css 적용하기

--- posts/forms.py
+++ posts/forms.py
@@ -8,6 +8,12 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ["title", "content"]  #'__all__'
+        widgets = {
+            "title": forms.TextInput(
+                attrs={"class": "title", "placeholder": "제목을 입력하세요"}
+            ),
+            "content": forms.Textarea(attrs={"placeholder": "내용을 입력하세요"}),
+        }
 
     # Form에서 유효성 검사하기 1. clean_data  2. formfield 사용하기
     def clean_title(self):
