f490a576442ef8a9979afa8ac3d1bf5e6c819ddd
Heo-JunHyeok <junhyeok_1996@naver.com>
2023-07-03 02:38:26 +0900

    form / 유효성 검사하기

--- posts/forms.py
+++ posts/forms.py
@@ -1,8 +1,17 @@
 from django import forms
 from .models import Post
+from .validators import validate_symbols
+from django.core.exceptions import ValidationError
 
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ["title", "content"]  #'__all__'
+
+    # Form에서 유효성 검사하기 1. clean_data  2. formfield 사용하기
+    def clean_title(self):
+        title = self.cleaned_data["title"]
+        if "*" in title:
+            raise ValidationError("*은 포함될 수 없습니다.")
+        return title
