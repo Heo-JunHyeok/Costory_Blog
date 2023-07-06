73cbaaf3ef2a56368cba25dc1be81e30e5719f85
Heo-JunHyeok <junhyeok_1996@naver.com>
2023-07-06 23:17:57 +0900

    generic view, create

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator
+
+# from django.views import View
+from django.views.generic import CreateView
+from django.urls import reverse
 from .models import Post
 from .forms import PostForm
 
@@ -24,15 +28,39 @@ def post_detail(request, post_id):
     return render(request, "posts/post_detail.html", {"post": post})
 
 
-def post_create(request):
-    if request.method == "POST":
-        post_form = PostForm(request.POST)
-        if post_form.is_valid():
-            new_post = post_form.save()
-            return redirect("post-detail", post_id=new_post.id)
-    else:
-        post_form = PostForm()
-    return render(request, "posts/post_form.html", {"form": post_form})
+# FBV
+# def post_create(request):
+#     if request.method == "POST":
+#         post_form = PostForm(request.POST)
+#         if post_form.is_valid():
+#             new_post = post_form.save()
+#             return redirect("post-detail", post_id=new_post.id)
+#     else:
+#         post_form = PostForm()
+#     return render(request, "posts/post_form.html", {"form": post_form})
+
+
+# CBV(View)
+# class PostCreateView(View):
+#     def get(self, request):
+#         post_form = PostForm()
+#         return render(request, "posts/post_form.html", {"form": post_form})
+
+#     def post(self, request):
+#         post_form = PostForm(request.POST)
+#         if post_form.is_valid():
+#             new_post = post_form.save()
+#             return redirect("post-detail", post_id=new_post.id)
+#         return render(request, "posts/post_form.html", {"form": post_form})
+
+
+class PostCreateView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = "posts/post_form.html"
+
+    def get_success_url(self):
+        return reverse("post-detail", kwargs={"post_id": self.objects.id})
 
 
 def post_update(request, post_id):
