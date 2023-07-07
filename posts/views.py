33d348de71296bdbd0438612342fe33956f127ce
Heo-JunHyeok <junhyeok_1996@naver.com>
2023-07-07 13:41:17 +0900

    +detail / CBV

--- posts/views.py
+++ posts/views.py
@@ -2,25 +2,14 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator
 
 # from django.views import View
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, DetailView
 from django.urls import reverse
 from .models import Post
 from .forms import PostForm
 
 
-# Create your views here.
 def index(request):
     return redirect("post-list")
-
-
-# def post_list(request):
-#     posts = Post.objects.all()
-#     paginator = Paginator(posts, 6)
-#     curr_page_num = request.GET.get("page")
-#     if curr_page_num is None:
-#         curr_page_num = 1
-#     page = paginator.page(curr_page_num)
-#     return render(request, "posts/post_list.html", {"page": page})
 
 
 class PostListView(ListView):
@@ -32,35 +21,11 @@ class PostListView(ListView):
     page_kwarg = "page"
 
 
-def post_detail(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
-    return render(request, "posts/post_detail.html", {"post": post})
-
-
-# FBV
-# def post_create(request):
-#     if request.method == "POST":
-#         post_form = PostForm(request.POST)
-#         if post_form.is_valid():
-#             new_post = post_form.save()
-#             return redirect("post-detail", post_id=new_post.id)
-#     else:
-#         post_form = PostForm()
-#     return render(request, "posts/post_form.html", {"form": post_form})
-
-
-# CBV(View)
-# class PostCreateView(View):
-#     def get(self, request):
-#         post_form = PostForm()
-#         return render(request, "posts/post_form.html", {"form": post_form})
-
-#     def post(self, request):
-#         post_form = PostForm(request.POST)
-#         if post_form.is_valid():
-#             new_post = post_form.save()
-#             return redirect("post-detail", post_id=new_post.id)
-#         return render(request, "posts/post_form.html", {"form": post_form})
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "posts/post_detail.html"
+    pk_url_kwarg = "post_id"
+    context_object_name = "post"
 
 
 class PostCreateView(CreateView):
@@ -91,3 +56,51 @@ def post_delete(request, post_id):
         return redirect("post-list")
     else:
         return render(request, "posts/post_confirm_delete.html", {"post": post})
+
+
+# ---------------------------------------------------------------------------------------
+
+# FBV(Funtion Based View)
+
+# def post_list(request):
+#     posts = Post.objects.all()
+#     paginator = Paginator(posts, 6)
+#     curr_page_num = request.GET.get("page")
+#     if curr_page_num is None:
+#         curr_page_num = 1
+#     page = paginator.page(curr_page_num)
+#     return render(request, "posts/post_list.html", {"page": page})
+
+
+# def post_detail(request, post_id):
+#     post = get_object_or_404(Post, id=post_id)
+#     return render(request, "posts/post_detail.html", {"post": post})
+
+
+# def post_create(request):
+#     if request.method == "POST":
+#         post_form = PostForm(request.POST)
+#         if post_form.is_valid():
+#             new_post = post_form.save()
+#             return redirect("post-detail", post_id=new_post.id)
+#     else:
+#         post_form = PostForm()
+#     return render(request, "posts/post_form.html", {"form": post_form})
+
+
+# --------------------------------------------------------------------------------------
+
+# CBV(import View)
+
+
+# class PostCreateView(View):
+#     def get(self, request):
+#         post_form = PostForm()
+#         return render(request, "posts/post_form.html", {"form": post_form})
+
+#     def post(self, request):
+#         post_form = PostForm(request.POST)
+#         if post_form.is_valid():
+#             new_post = post_form.save()
+#             return redirect("post-detail", post_id=new_post.id)
+#         return render(request, "posts/post_form.html", {"form": post_form})
